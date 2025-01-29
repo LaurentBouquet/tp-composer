101e23270f92b04cd1adb396af34f5794a260477
Laurent Bouquet <bouquet@ndlp.fr>
2025-01-29 16:07:31 +0100

    Connexion à la BD

--- main.php
+++ main.php
@@ -8,6 +8,15 @@ use Monolog\Handler\StreamHandler;
 use PHPMailer\PHPMailer\PHPMailer;
 use Symfony\Component\Dotenv\Dotenv;
 
+function addFighter($l){
+
+    // TODO ajouter le code pour insérer un combattant dans la base de données
+    
+    $l->info("The figther is added to the database");
+}
+
+
+
 
 // Premiers pas avec Composer
 print("Premiers pas avec Composer \n\n");
@@ -21,31 +30,12 @@ $log->pushHandler(new StreamHandler('log/info.log', Level::Warning));
 $dotenv = new Dotenv();
 $dotenv->load(__DIR__.'/.env');
 
-$log->info("Démarrage de l'envoi d'un mail");
-$mail = new PHPMailer(true);
+addFighter($log);
 
-try {
-    $mail = new PHPMailer(true);
 
-    $mail->isSMTP();
-    $mail->Host = $_ENV['SMTP_HOST'];
-    $mail->SMTPAuth = true;
-    $mail->Username = $_ENV['SMTP_USERNAME'];
-    $mail->Password = $_ENV['SMTP_PASSWORD'];
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-    $mail->Port = $_ENV['SMTP_PORT']; //587; // ou 465
-    
-    $mail->setFrom($_ENV['SMTP_USERNAME'], $_ENV['SMTP_NAME']);
-    $mail->addAddress($_ENV['SMTP_USERNAME'], $_ENV['SMTP_NAME']);
-    $mail->Subject = "Ceci est un test";
-    $mail->Body    = "Ceci est un test d'envoi d'un mail avec Infomaniak et Composer.";
 
-    $mail->send();
-    $log->info('Message has been sent');
-} catch (Exception $e) {
-    $log->error("Message could not be sent. Smtp server: {$_ENV['SMTP_HOST']} and Mailer Error: {$mail->ErrorInfo}");
-}
 
-$log->info("The mail is sent");
+
+
 
 
